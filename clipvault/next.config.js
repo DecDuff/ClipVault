30496925fca9f4bff1a051145b61e0ed80036d72
DecDuff <Declanduffyhyrule@icloud.com>
2026-03-24 14:59:33 -0400

    Update next.config.js

--- clipvault/next.config.js
+++ clipvault/next.config.js
@@ -6,6 +6,8 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
 
 module.exports = nextConfig;
